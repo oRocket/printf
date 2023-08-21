8bf860834d6f210cc01588189b8dc2ded038c4d0
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-08-21 03:11:08 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @flags_t: The operator
+ * @f: The function associated
+ */
+typedef struct print
+{
+	char *op;
+	int (*f)(va_list, char *, unsigned int);
+} op_t;
+
+/**
+ * struct op - Struct op
+ *
+ * @flags_t: The operator
+ * @f: The function associated
+ */
+typedef struct 
+{
+	int d;
+	int i;
+} flags_t;
+
+int print_integer(va_list args);
+int _printf(const char *format, ...);
+int _putc(char c);
+
+#endif
